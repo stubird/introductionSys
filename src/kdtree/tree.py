5c7c9c16a029a7f9e3fee3868f66b90d413c24e5
stubird <falcon_dt@yeah.net>
2018-01-15 19:27:59 +0800

    enable tree research

--- src/kdtree/tree.py
+++ src/kdtree/tree.py
@@ -17,19 +17,27 @@ from random import random
 
 import numpy as np
 
+import logging
+
+
+
 class Tree:
 
     def __init__(self, dataSet):
+        self.name = 'Tree'
+        self.nodeNum = 0
         self.root = self.buildTreeByData(dataSet)
+
 
     def buildTreeByData(self, dataSet):
         root = branch(self, dataSet, dimension=0)
         root.getConstraint(root)
         return root
 
+
 class branch:
 
-    def __init__(self, tree, dataSet, dimension=0, parent = None):
+    def __init__(self, tree, dataSet, dimension=0, parent=None, type = 'root'):
         """
         :param tree: tree this branch belong of
         :param dataSet: dataSet the branch to build,
@@ -43,7 +51,7 @@ class branch:
             array [3,1,1] Constraint is [3,1] and class code is 1
         :param dimension: dimension of the branch
         """
-        #print ("new branch,param: dataSet:",dataSet, " , dimension = ", dimension)
+        #logger.debug ("new branch,param: dataSet:%s, dimension = %d",dataSet, dimension)
         if len(dataSet) > 1:
             self.buildSonByData(tree, dataSet, dimension)
         else:
@@ -52,8 +60,12 @@ class branch:
 
         if parent == None:
             self.parent = 'root'
+            self.name = type + tree.nodeNum.__str__()
+            tree.nodeNum = tree.nodeNum + 1
         else:
             self.parent = parent
+            self.name = type + tree.nodeNum.__str__()
+            tree.nodeNum = tree.nodeNum + 1
 
         self.coordinate = self.getCoordinate(dataSet, dimension)
         self.value = self.coordinate[-1]
@@ -68,23 +80,19 @@ class branch:
         leftSetIndices = leftIndices[:-1]
         leftSet = [dataSet[x] for x in leftSetIndices]
         if len(leftSet) == 0:
-            #print("have no left")
             self.left = None
         else:
-            #print("build left")
-            self.left = branch(tree, leftSet, dimension=nextDimension, parent=self)
+            self.left = branch(tree, leftSet, dimension=nextDimension, parent=self, type = 'left')
 
         rightIndices = range(len(dataSet))
         rightIndices = set(rightIndices).difference(set(leftIndices))
         rightSet = [dataSet[x] for x in rightIndices]
         if len(rightSet) == 0:
-            #print("have no right")
             self.right = None
         else:
-            #print("build right")
-            self.right = branch(tree, rightSet, dimension=nextDimension, parent=self)
+            self.right = branch(tree, rightSet, dimension=nextDimension, parent=self, type = 'right')
 
-    def getConstraint(self, root, dimension = 0):
+    def getConstraint(self, root, dimension=0):
         """calculate every point constraint. We can also say calculate the
             super space the point hold
         """
@@ -95,11 +103,11 @@ class branch:
         if root.left != None:
             root.left.space = [x[:] for x in root.space]
             root.left.space[dimension][1] = border
-            self.getConstraint(root.left, dimension = nextDimension)
+            self.getConstraint(root.left, dimension=nextDimension)
         if root.right != None:
             root.right.space = [x[:] for x in root.space]
             root.right.space[dimension][0] = border
-            self.getConstraint(root.right, dimension = nextDimension)
+            self.getConstraint(root.right, dimension=nextDimension)
 
     def getCoordinate(self, dataSet, dimension):
         """
@@ -107,47 +115,43 @@ class branch:
         choose median number in the dimension of the data set
         """
         dataindex = self.getLeftIndices(dataSet, dimension)
-        #return coordinate array
+        # return coordinate array
         return dataSet[dataindex[-1]][:]
 
     def getLeftIndices(self, dataSet, dimension):
         array = [x[dimension] for x in dataSet]
         sortIndex = np.argsort(array, axis=0)
-        #get median number
+        # get median number
         midnum = int(len(sortIndex) / 2) + len(sortIndex) % 2
-        #get median number index
+        # get median number index
         dataindex = sortIndex[0:midnum]
         return dataindex
 
-
-    def printNode(self, node, pad = '', dimension = 0):
-        global index4prt
-        index = index4prt + 1
-        index4prt = index4prt + 1
-        print ('node',index,' is : ', node.coordinate, 'space:', node.space, 'dimension = ',dimension)
+    def printNode(self, node, pad='', dimension=0):
         pad = pad + '|    '
+        logger.debug('%s%s is :%s ,space:%s,dimension = %d',pad, node.name, node.coordinate,node.space, dimension)
         if node.left == None and node.right == None:
-            print (pad, 'node',index,' is leave')
+            logger.debug('%s%s is leave',pad, node.name)
             return
 
         nextDimension = self.getNextDimension(dimension, len(node.coordinate) - 2)
         if node.left != None:
-            print (pad, 'node',index ,' left is ',end="" )
-            self.printNode(node.left,pad = pad, dimension = nextDimension)
+            logger.debug('%s%s left is ',pad, node.name)
+            self.printNode(node.left, pad=pad, dimension=nextDimension)
         else:
-            print (pad, 'node',index,' have no left ')
+            logger.debug('%s%s have no left ',pad, node.name)
 
         if node.right != None:
-            print(pad, 'node',index,' right is ',end="")
-            self.printNode(node.right, pad = pad, dimension = nextDimension)
+            logger.debug('%s%s right is ', pad, node.name)
+            self.printNode(node.right, pad=pad, dimension=nextDimension)
         else:
-            print(pad, 'node',index,' have no right ')
+            logger.debug('%s%s have no right ', pad, node.name)
         return
 
     def getNextDimension(self, dimension, gate):
         return dimension + 1 if dimension < gate else 0
 
-    def verifyTree(self, root, dimension = 0):
+    def verifyTree(self, root, dimension=0):
         maxDimension = len(self.coordinate)
         next = self.getNextDimension(dimension, maxDimension - 2)
 
@@ -162,12 +166,13 @@ class branch:
             sign1 = True if root.right.coordinate[dimension] > root.coordinate[dimension] else False
             self.verifyTree(root.right, next)
 
-        print("1", sign1, " 2", sign2)
+        logger.debug("1", sign1, " 2", sign2)
 
     def findPointSpace(self, parent, point):
+        # find the space match to the point
         dimension = 0
         while True:
-            if point[dimension] > parent.coordinate[dimension]:
+            if point.coordinate[dimension] > parent.coordinate[dimension]:
                 if parent.right != None:
                     parent = parent.right
                 else:
@@ -180,38 +185,55 @@ class branch:
             dimension = self.getNextDimension(dimension, len(parent.coordinate) - 2)
 
     def findNearestPoint(self, root, point):
-        spacepoint = root.findPointSpace(root, point)
+        bestPoint = root.findPointSpace(root, point)
         # range
-        superSphere = [[min([point.coordinate[y] - point.coordinate[y] + spacepoint.coordinate[y],
-                    point.coordinate[y] + point.coordinate[y] - spacepoint.coordinate[y]]),
-                    max([point.coordinate[y] - point.coordinate[y] + spacepoint.coordinate[y],
-                    point.coordinate[y] + point.coordinate[y] - spacepoint.coordinate[y]])]
-                   for y in range(len(point) - 1)]
+        superSphere = [[min([bestPoint.coordinate[y],
+                             2 * point.coordinate[y] - bestPoint.coordinate[y]]),
+                        max([bestPoint.coordinate[y],
+                             2 * point.coordinate[y] - bestPoint.coordinate[y]])]
+                       for y in range(len(point.coordinate) - 1)]
 
-        sibling = spacepoint.findSibling()
-        distance = [abs(spacepoint.space[i] - point.space[i]) for i in range(len(spacepoint.coordinate) - 1)]
+        distance = [abs(bestPoint.coordinate[i] - point.coordinate[i]) for i in range(len(bestPoint.coordinate) - 1)]
         distance = sum(distance)
 
-        while sibling.parent != 'root' and sibling.parent.parent != 'root':
+        if bestPoint.left != None:
             bestPoint, superSphere, distance = \
-                sibling.findBestFromOneNode(self, point, spacepoint, superSphere, distance)
+                bestPoint.left.testNearPoint(point, bestPoint, superSphere, distance)
+        elif bestPoint.right != None:
             bestPoint, superSphere, distance = \
-                sibling.parent.testNearPoint(bestPoint, superSphere, distance)
-            sibling = sibling.parent.parent.left\
-                if sibling != sibling.parent.parent.left else sibling.parent.parent.right
+                bestPoint.right.testNearPoint(point, bestPoint, superSphere, distance)
+
+        bestPoint, superSphere, distance, sibling = bestPoint.findSibling(point, bestPoint, superSphere, distance)
+        logger.debug("begin find nearest:\n\
+                        bestpoint: %s,distance:%d ,sibing:%s", bestPoint.name, distance, sibling.name)
+        while True:
+            bestPoint, superSphere, distance = \
+                sibling.findBestFromOneNode(point, bestPoint, superSphere, distance)
+            bestPoint, superSphere, distance = \
+                sibling.parent.testNearPoint(point, bestPoint, superSphere, distance)
+            if sibling.parent == 'root' or sibling.parent.parent == 'root':
+                break
+            sibling = sibling.parent.parent.left \
+                if sibling.parent != sibling.parent.parent.left else sibling.parent.parent.right
+            logger.debug("loop find nearest:\n\
+                bestpoint: %s,distance:%d ,sibing:%s",bestPoint.name,  distance, sibling.name)
+
+        return bestPoint, distance
 
     def findBestFromOneNode(self, targetPoint, bestPoint, superSphere, distance):
         if self.isOverlap(superSphere) == False:
+            logger.debug("think overlap, ", self.name, "self,space:",self.space,"superSphere",superSphere)
             return bestPoint, superSphere, distance
 
-        selfDistance = [abs(self.space[i] - targetPoint.space[i]) for i in range(len(self.coordinate) - 1)]
+        selfDistance = [abs(self.coordinate[i] - targetPoint.coordinate[i]) for i in range(len(self.coordinate) - 1)]
         selfDistance = sum(selfDistance)
+        logger.debug("%s's distance = %d", self.name, selfDistance)
         if selfDistance < distance:
             distance = selfDistance
             bestPoint = self
             superSphere = [[min([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]]),
-                        max([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]])]
-                       for y in range(len(bestPoint.coordinate) - 1)]
+                            max([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]])]
+                           for y in range(len(bestPoint.coordinate) - 1)]
 
         if self.left != None:
             bestPoint, superSphere, distance = \
@@ -223,52 +245,90 @@ class branch:
         return bestPoint, superSphere, distance
 
     def testNearPoint(self, targetPoint, bestPoint, superSphere, distance):
-        selfDistance = [abs(self.space[i] - targetPoint.space[i]) for i in range(len(self.coordinate) - 1)]
+        selfDistance = [abs(self.coordinate[i] - targetPoint.coordinate[i]) for i in range(len(self.coordinate) - 1)]
         selfDistance = sum(selfDistance)
         if selfDistance < distance:
             distance = selfDistance
             bestPoint = self
             superSphere = [[min([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]]),
-                        max([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]])]
-                       for y in range(len(bestPoint.coordinate) - 1)]
+                            max([bestPoint.coordinate[y], 2 * targetPoint.coordinate[y] - bestPoint.coordinate[y]])]
+                           for y in range(len(bestPoint.coordinate) - 1)]
         return bestPoint, superSphere, distance
 
     def loopForSons(self, point):
         if self.left != None and self.left.isOverlap(point):
             self.left.testNearPoint()
 
-    def findSibling(self):
+    def findSibling(self,point, bestPoint, superSphere, distance):
         ret = 'over'
         if self.parent != 'root' and self.parent.left != None and self.parent.left != self:
-            return self.parent.left
+            logger.debug("find parent's left")
+            return bestPoint, superSphere, distance,self.parent.left
         elif self.parent != 'root' and self.parent.right != None and self.parent.right != self:
-            return self.parent.right
+            logger.debug("find parent's right")
+            return bestPoint, superSphere, distance,self.parent.right
         elif self.parent != 'root':
-            self.parent.findSibing()
-
-        return ret
+            logger.debug("parent have no other sibling, find up layer")
+            bestPoint, superSphere, distance = self.parent.testNearPoint(point, bestPoint, superSphere, distance)
+            bestPoint, superSphere, distance, ret = self.parent.findSibling(point, bestPoint, superSphere, distance)
+        return bestPoint, superSphere, distance, ret
 
     def isOverlap(self, node):
         for x in range(len(self.coordinate) - 1):
-            if (self.space[0] > node[0] and self.space[0] < node[1]) or \
-                    (self.space[0] > node[0] and self.space[0] < node[1]):
+            if (self.space[x][0] > node[x][0] and self.space[x][0] < node[x][1]) or \
+                    (self.space[x][1] > node[x][0] and self.space[x][1] < node[x][1]) or \
+                    (self.space[x][0] < node[x][0] and self.space[x][1] > node[x][1]):
                 return True
 
         return False
 
-index4prt = 0
-
 def test():
 
-    dataSet = [[int(random()*100) for x in range(3)] for y in range(2000)]
+
+    dataSet = [[6, 28, 73, 55, 4, 44, 38, 29], [38, 91, 85, 25, 72, 11, 48, 0], [74, 40, 74, 50, 72, 32, 56, 77], [30, 59, 51, 61, 20, 50, 30, 54], [49, 33, 78, 24, 65, 18, 91, 71], [14, 92, 95, 59, 24, 94, 24, 26], [96, 58, 56, 56, 89, 16, 57, 25], [6, 66, 92, 4, 89, 78, 69, 73], [17, 69, 88, 62, 27, 0, 72, 65], [39, 68, 96, 41, 76, 66, 71, 19], [9, 84, 2, 48, 96, 22, 93, 95], [8, 11, 81, 11, 67, 98, 54, 56], [48, 31, 80, 68, 2, 1, 25, 87], [17, 28, 6, 46, 19, 57, 50, 67], [15, 39, 56, 20, 67, 90, 75, 37], [85, 61, 34, 71, 32, 33, 84, 81], [83, 50, 3, 41, 51, 8, 75, 89], [30, 99, 10, 89, 16, 35, 84, 17], [67, 10, 62, 31, 73, 14, 33, 3], [13, 45, 37, 60, 57, 59, 72, 68]]
+    dataSet = [[int(random() * 100) for x in range(50)] for y in range(2000)]
+
 
     tree = Tree(dataSet)
-    #tree.root.printNode(tree.root)
+    tree.root.printNode(tree.root)
 
-    #tree.root.verifyTree(tree.root)
+    # tree.root.verifyTree(tree.root)
 
-    point = branch(tree, [22,5,4])
 
-    tree.root.findNearestPoint(tree.root, point)
+    point = branch(tree, [[82, 63, 97, 4, 26, 74, 74, 68]], type = 'test')
+    point = branch(tree, [[int(random() * 100) for x in range(50)]], type = 'test')
 
-test()
+    bestPoint, distance = tree.root.findNearestPoint(tree.root, point)
+
+    space = tree.root.findPointSpace(tree.root, point)
+
+    selfDistance = float('inf')
+    coord = []
+    for vector in dataSet:
+        tmpDistance = [abs(point.coordinate[i] - vector[i]) for i in range(len(point.coordinate) - 1)]
+        tmpDistance = sum(tmpDistance)
+        if tmpDistance < selfDistance:
+            selfDistance = tmpDistance
+            coord = vector
+
+    #print("point", point.name,"bestPoint:", bestPoint.name, ", distance:", distance)
+    #print("real distance = ", selfDistance, coord,", space:", space.name)
+    if distance == selfDistance:
+        return True
+    else:
+        print(dataSet)
+        print("point", point.name,"bestPoint:", bestPoint.name, ", distance:", distance,\
+            "real distance = ", selfDistance, "coord",coord,"point",point.coordinate,", space:", space.name)
+        return False
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+count = 200
+miss = 0
+while count > 0:
+    if False == test():
+        break
+        miss = miss + 1
+    count = count - 1
+
+print(miss)
